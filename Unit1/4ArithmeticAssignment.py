fec25182bedca535214d692ab5bf77d1f926a8e9
matthew <26matthewcranesousa@usd266.com>
2024-04-04 12:53:49 -0500

    CONDITIONALS

--- Unit1/4ArithmeticAssignment.py
+++ Unit1/4ArithmeticAssignment.py
@@ -1,23 +1,64 @@
 print("########## 1.4.1 ##########")
-
+#Write a code which asks the user for a number. 
+#The program then prints out the number multiplied 
+#by five.
+number_str = (input("enter a number here: "))
+number = int(number_str)
+number2 = 5
+sum = number*number2
+print(number, "times", number2, "is", sum)
 print("########## 1.4.2 ##########")
-
+#Write code which asks the user for their name and
+#year of birth.
+name = input("What is your name? ")
+year_born = input("which year were you born? ")
+year = int(year_born)
+current_year = 2024
+age = (current_year-year)
+print("hello", name + ",", "you will be", age, "at the end of the year 2024")
 print("########## 1.4.3 ##########")
-
+#Write code which asks the user for a number of days.
+#The code then prints out the number of seconds in 
+# the amount of days given.
+days_gone_by = input("How many days? ")
+days = int(days_gone_by)
+seconds = 86400
+seconds_in_day = (seconds*days)
+print("Seconds in that many days:", seconds_in_day)
 print("########## 1.4.4 ##########")
 # Fix the code
 number = int(input("Please type in the first number: "))
-number = int(input("Please type in the second number: "))
-number = int(input("Please type in the third number: "))
-
-product = number * number * number
+number2 = int(input("Please type in the second number: "))
+number3 = int(input("Please type in the third number: "))
+product = number * number2 * number3
 
 print("The product is", product)
 
 print("########## 1.4.5 ##########")
-
+number = int(input("enter first number: "))
+number2 = int(input("enter second number: "))
+sum = number + number2
+product = number * number2
+print("the sum of ther numbers:", sum)
+print("the product of the numbers:", product)
 print("########## 1.4.6 ##########")
-
+number = int(input("number 1: "))
+number2 = int(input("number 2: "))
+number3 = int(input("number 3: "))
+number4 = int(input("number 4: "))
+sum = number + number2 + number3 + number4
+mean = sum / 4
+print("The sum of the numbers is", sum, "and the mean is", mean)
 print("########## 1.4.7 ##########")
+schoolLunch = float(input("How many times a week do you eat at the student cafeteria? "))
+price = float(input("The price of typical student lunch? "))
+groceries = float(input("How much money do you spend on groceries in a week? "))
+total_spent = groceries + schoolLunch * price
+daily = total_spent/ 7
+weekly = daily* 7
+print("Average food expenditure:")
+print("Daily:", "$" + daily)
+print("Weekly:", "$" + weekly)
 
 print("########## 1.4.8 ##########")
+
--- Unit1/5ConditionalsAssignment.py
+++ Unit1/5ConditionalsAssignment.py
@@ -1,29 +1,88 @@
 
 print("########## 1.5.1 ##########")
-
+number = int(input("Please type a number: "))
+if number == 1984:
+    print("Orwell")
 print("########## 1.5.2 ##########")
-
+number = int(input("Please type in a number: "))
+if number < 0:
+    av = number*-1
+    print("The absolute value of this number is", av)
+if number > 0:
+    print("The absolute value of this is", number)
 print("########## 1.5.3 ##########")
-
+cost = 5.90
+name = input("What is your name? ")
+if name == str("Jerry"):
+    print("Next please!")
+if name != str("Jerry"):
+    portion = int(input("How many portions of soup? "))
+    total = cost*portion
+    print("The total cost is", total)
+    print("Next please!")
 print("########## 1.5.4 ##########")
-
+number = int(input("Type in a number: "))
+if number < 1000:
+    print("This number is smaller than 1000")
+if number < 100:
+    print("This number is less than 100")
+if number < 10:
+    print("This number is less than 10")
+print("Thank you!")
 print("########## 1.5.5 ##########")
-
+number = int(input("Number 1: "))
+number2 = int(input("Number 2: "))
+operation = input("Operation: ")
+if operation == str("add"):
+    add = number+number2
+    print(number, "+", number2, "=", add)
+if operation == str("multiply"):
+    multiply = number*number2
+    print(number, "*", number2, "=", multiply)
+if operation == str("subtract"):
+    subtract = number-number2
+    print(number, "-", number2, "=", subtract)
 print("########## 1.5.6 ##########")
-
+temperature = int(input("Type in a temperature (F): "))
+celsius = (temperature-32) * 5/9
+print(temperature, "degrees Fahrenheit equals", celsius, "degrees Celsius")
+if celsius < 0:
+    print("Brr! It's cold in here!")
 print("########## 1.5.7 ##########")
-
+wage = float(input("What is your hourly wage? "))
+hours = float(input("How many hours have you worked? "))
+day = input("What day of the week is it? ")
+if day != str("Sunday"):
+    dailywage = wage*hours
+    print("daily wages:", dailywage)
+if day == str("Sunday"):
+    dailywage = (wage*2)*hours
+    print("daily wage:", dailywage)
 print("########## 1.5.8 ##########")
 # Fix the program
 points = int(input("How many points are on your card? "))
-if points < 100:
-    points *= 1.1
-    print("Your bonus is 10 %")
-
 if points >= 100:
     points *= 1.15
     print("Your bonus is 15 %")
+if points < 100:
+    points *=1.1
+    print("Your bonus is 10 %")
 
 print("You now have", points, "points")
 
 print("########## 1.5.9 ##########")
+print("What is the weather forecast for tomorrow?")
+temperature = int(input("Temperature: "))
+rain = input("Will it rain (yes/no): ")
+if temperature > 20:
+    print("Wear jeans and a T-shirt")
+if temperature <= 20:
+    print("Wear jeans and a T-shirt")
+    print("I reccommend a sweater as well")
+if temperature <= 10:
+    print("take a jacket with you")
+if temperature <= 5:
+    print("Make it a warm coat, actually")
+    print("I think gloves are in order")
+if rain == str("yes"):
+    print("Don't forget your umbrella!")
