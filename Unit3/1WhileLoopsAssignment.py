04f26bfcacdfef4e42781ca8da40c4b8a8dec46d
matthew <26matthewcranesousa@usd266.com>
2024-04-19 11:22:29 -0500

    3.1 done - matt

--- Unit3/1WhileLoopsAssignment.py
+++ Unit3/1WhileLoopsAssignment.py
@@ -1,21 +1,42 @@
 # ========== 3.1.1 ==========
-
-
+# number = 4
+# while number < 30:
+#     print(number)
+#     number += 2
 # ========== 3.1.2 ==========
-
 # print("Are you ready?")
 # number = int(input("Please type in a number: "))
-# while number = 0:
-# print(number)
+# while number > 0:
+#     print(number)
+#     number -= 1
 # print("Go!")
-
 # ========== 3.1.3 ==========
-
-
+# number = int(input("Insert number: "))
+# integers = 2
+# print("Upper limit:", number)
+# while integers < number and integers > 0:
+#     print(integers)
+#     integers += 1
 # ========== 3.1.4 ==========
-
-
+# up = int(input("Insert number: "))
+# integers = 1
+# print("Upper limit:", up) 
+# while integers <= up and integers > 0:
+#     print(integers)
+#     integers *= 2
 # ========== 3.1.5 ==========
-
-
+# upper = int(input("Upper limit: "))
+# base = int(input("Base: "))
+# integers = 1
+# while integers <= upper and integers > 0:
+#     print(integers)
+#     integers *= base
 # ========== 3.1.6 ==========
+# Limit = int(input("Limit: "))
+# currentNumber = 1
+# total = 0
+# while total < Limit:
+#     total = currentNumber + total
+#     currentNumber += 1
+
+# print(total)
